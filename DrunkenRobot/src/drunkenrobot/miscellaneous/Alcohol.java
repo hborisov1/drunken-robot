dc568500084848a7b73c93a12660764cbc39cc76
hborisov1 <hborisov1@gmail.com>
2012-12-20 16:30:08 +0200

    Alcohol class created

---
+++ DrunkenRobot/src/drunkenrobot/miscellaneous/Alcohol.java
@@ -0,0 +1,29 @@
+package drunkenrobot.miscellaneous;
+
+public class Alcohol {
+
+	private String alcoholType;
+	private String alcoholBrand;
+	private double alcoholPercentage;
+
+	public Alcohol(String alcoholType, String alcoholBrand,
+			double alcoholPercentage) {
+		super();
+		this.alcoholType = alcoholType;
+		this.alcoholBrand = alcoholBrand;
+		this.alcoholPercentage = alcoholPercentage;
+	}
+
+	public String getAlcoholType() {
+		return alcoholType;
+	}
+
+	public String getAlcoholBrand() {
+		return alcoholBrand;
+	}
+
+	public String getAlcoholPercentage() {
+		return (double) Math.round(alcoholPercentage * 10) / 10 + "%";
+	}
+
+}
