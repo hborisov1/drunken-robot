9e7805e083231a16729bbce9e8d221a72ed61cc8
hborisov1 <hborisov1@gmail.com>
2012-12-21 13:08:10 +0200

    Added power robot, that communicates with voltron

--- DrunkenRobot/src/drunkenrobot/MainApp.java
+++ DrunkenRobot/src/drunkenrobot/MainApp.java
@@ -9,14 +9,22 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		Pub pub = new Pub(4);
+		
 		Robot voltron = new Robot("Voltron");
 		Robot powerRobot = new Robot("PowerRobot");
 		
-		Alcohol bendersAlcohol = new Alcohol("Beer", "Ariana", 5);
+		Alcohol voltronsAlcohol = new Alcohol("Beer", "Ariana", 5);
+		Alcohol powerRobotsAlcohol = new Alcohol("Whiskey", "Johney Walker", 40.2);
 
 		pub.getTablesByIndex(1).addRobot(voltron);
-		voltron.setAlcohol(bendersAlcohol);
+		pub.getTablesByIndex(1).addRobot(powerRobot);
+		
+		voltron.setAlcohol(voltronsAlcohol);
 		voltron.setPhrase("I will pull my light sword and cut your head");
+		
+		powerRobot.setAlcohol(powerRobotsAlcohol);
+		powerRobot.setPhrase("‘ÛˆÍ you, voltron");
+		
 		pub.getTablesByIndex(1).listen();
 		
 		
